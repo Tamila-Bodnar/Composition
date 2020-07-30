3ae354d9b6d07bf2d7ce14108aeecc987d9f9915
Tamila-Bodnar <tamila.bodnar@gmail.com>
2020-07-30 08:46:34 +0300

    exercises

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const pipe = (...fns) => x => null;
+const pipe = (...fns) => {
+  for (const arg of fns) {
+    if (typeof arg !== 'function') {
+      throw new Error('not functions or digits');
+    }
+  }
+  return x => fns.reduce((v, f) => f(v), x);
+};
 
 module.exports = { pipe };
--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,25 @@
 'use strict';
 
-const compose = (...fns) => x => null;
+const compose = (...fns) => {
+  const handErrors = [];
+  const findError = x => {
+    if (fns.length === 0) return x;
+    let result = x;
+    try {
+      for (let i = fns.length - 1; i >= 0; i--) {
+        result = fns[i](result);
+      }
+      return result;
+    } catch (error) {
+      for (const handError of handErrors) {
+        handError(error);
+      }
+    }
+  };
+  findError.on = (name, handError) => {
+    if (name === 'error') handErrors.push(handError);
+  };
+  return findError;
+};
 
 module.exports = { compose };
